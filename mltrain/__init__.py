c000105b37408f90ba66bb0f94d98d89d99f92cf
CS041 Faatih <22oo1cso41faatih@gmail.com>
2024-11-26 23:58:11 +0530

    Fixed Errors in init.py file

--- mltrain/__init__.py
+++ mltrain/__init__.py
@@ -1,10 +1,14 @@
-# Use Shortcuts to import unique Models..
-from supervised.DecisionTree import DecisionTree
-from supervised.KernelSVM import KernelSVM
-from supervised.KNN import KNN
-from supervised.LinearRegression import LinearRegression
-from supervised.LogisticRegression import LogisticRegression
-from supervised.NaiveBayes import NaiveBayes
-from supervised.RandomForest import RandomForest
-from unsupervised.KMeans import KMeans
-from unsupervised.DBSCAN import DBSCAN
+# init file for main branch
+# eg: from mltrain import KNN, DecisionTree, KMeans
+
+from .supervised.KNN import KNN
+from .supervised.DecisionTree import DecisionTree
+from .supervised.KernelSVM import KernelSVM
+from .supervised.LinearRegression import LinearRegression
+from .supervised.LogisticRegression import LogisticRegression
+from .supervised.NaiveBayes import NaiveBayes
+from .supervised.RandomForest import RandomForest
+
+from .unsupervised.KMeans import KMeans
+from .unsupervised.DBSCAN import DBSCAN
+from .unsupervised.PCA import PCA
